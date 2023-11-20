6bbcc8638ad8ee9f40b2c2901935fbce01c3a172
Rebecca Robison <rebeccahrobison@outlook.com>
2023-11-20 15:57:52 -0600

    Added TimeInStock to class, ability to see average time in stock, average time before sale, and busiest sale hours

--- Product.cs
+++ Product.cs
@@ -1,9 +1,30 @@
 public class Product
 {
-    public string Name { get; set; }
-    public decimal Price { get; set; }
-    public bool Sold { get; set; }
-    public DateTime StockDate { get; set; }
-    public int ManufactureYear { get; set; }
-    public double Condition { get; set; }
+  public string Name { get; set; }
+  public decimal Price { get; set; }
+  public DateTime? SoldOnDate { get; set; }
+  public DateTime StockDate { get; set; }
+  public TimeSpan TimeInStock
+  {
+    get
+    {
+      DateTime lastDay = SoldOnDate != null ? (DateTime)SoldOnDate : DateTime.Now;
+      return lastDay - StockDate;
+    }
+    // get
+    // {
+    //   DateTime lastDay;
+    //   if (SoldOnDate != null)
+    //   {
+    //     lastDay = (DateTime)SoldOnDate;
+    //   }
+    //   else
+    //   {
+    //     lastDay = DateTime.Now;
+    //   }
+    //   return lastDay - StockDate;
+    // }
+  }
+  public int ManufactureYear { get; set; }
+  public double Condition { get; set; }
 }
--- Program.cs
+++ Program.cs
@@ -1,4 +1,6 @@
-﻿using System.Net.WebSockets;
+﻿using System.Linq;
+using System.Net;
+using System.Net.WebSockets;
 
 List<Product> products = new List<Product>()
 {
@@ -6,8 +8,8 @@ List<Product> products = new List<Product>()
   {
     Name= "Football",
     Price = 15.00M,
-    Sold = false,
-    StockDate = new DateTime(2022, 10, 20),
+    SoldOnDate = null,
+    StockDate = new DateTime(2022, 11, 20),
     ManufactureYear = 2010,
     Condition = 4.2
   },
@@ -15,7 +17,7 @@ List<Product> products = new List<Product>()
   {
     Name = "Hockey Stick",
     Price = 12.00M,
-    Sold = false,
+    SoldOnDate = null,
     StockDate = new DateTime(2022, 10, 20),
     ManufactureYear = 2011,
     Condition = 3.2
@@ -24,7 +26,7 @@ List<Product> products = new List<Product>()
   {
     Name = "Baseball Bat",
     Price = 20.00M,
-    Sold = true,
+    SoldOnDate = new DateTime(2023, 11, 19, 10, 10, 00),
     StockDate = new DateTime(2022, 10, 20),
     ManufactureYear = 2012,
     Condition = 4.5
@@ -33,7 +35,7 @@ List<Product> products = new List<Product>()
   {
     Name = "Shin guards",
     Price = 19.00M,
-    Sold = false,
+    SoldOnDate = null,
     StockDate = new DateTime(2022, 10, 20),
     ManufactureYear = 2018,
     Condition = 4.9
@@ -43,7 +45,7 @@ List<Product> products = new List<Product>()
   {
     Name = "Athletic socks",
     Price = 6.00M,
-    Sold = false,
+    SoldOnDate = null,
     StockDate = new DateTime(2023, 11, 2),
     ManufactureYear = 2019,
     Condition = 5.0
@@ -53,7 +55,7 @@ List<Product> products = new List<Product>()
   {
     Name = "Running shorts",
     Price = 24.99M,
-    Sold = true,
+    SoldOnDate = new DateTime(2023, 10, 30, 2, 30, 00),
     StockDate = new DateTime(2023, 10, 22),
     ManufactureYear = 2022,
     Condition = 4.8
@@ -71,7 +73,8 @@ while (choice != "0")
                     0. Exit
                     1. View All Products
                     2. View Product Details
-                    3. View Latest Products");
+                    3. View Latest Products
+                    4. Monthly Sales Report");
   choice = Console.ReadLine();
   if (choice == "0")
   {
@@ -89,25 +92,30 @@ while (choice != "0")
   {
     ViewLatestProducts();
   }
+  else if (choice == "4")
+  {
+    MonthlySalesReport();
+  }
+  else if (choice == "5")
+  {
+    AddProduct();
+  }
 }
-
-
 
 
 
 
 void ViewProductDetails()
 {
-  // ListProducts();
-
   Product chosenProduct = null;
-
+  ListProducts();
+  int response = -1;
   while (chosenProduct == null)
   {
     Console.WriteLine("Please enter a product number: ");
     try
     {
-      int response = int.Parse(Console.ReadLine().Trim());
+      response = int.Parse(Console.ReadLine().Trim());
       chosenProduct = products[response - 1];
     }
     catch (FormatException)
@@ -126,13 +134,19 @@ void ViewProductDetails()
   }
 
   DateTime now = DateTime.Now;
-  TimeSpan timeInStock = now - chosenProduct.StockDate;
 
   Console.WriteLine(@$"You chose: 
   {chosenProduct.Name}, which costs {chosenProduct.Price} dollars.
   It is {now.Year - chosenProduct.ManufactureYear} years old.
   Its condition is rated {chosenProduct.Condition}.
-  It {(chosenProduct.Sold ? "is not available." : $"has been in stock for {timeInStock.Days} days.")}");
+  It {(chosenProduct.SoldOnDate != null ? "is not available." : $"has been in stock for {chosenProduct.TimeInStock} days.")}");
+
+  Console.WriteLine("Would you like to mark the item as sold? Y/N");
+  string userInput = Console.ReadLine();
+  if (userInput == "Y")
+  {
+    MarkAsSold(response);
+  }
 }
 
 void ListProducts()
@@ -140,7 +154,7 @@ void ListProducts()
   decimal totalValue = 0.0M;
   foreach (Product product in products)
   {
-    if (!product.Sold)
+    if (product.SoldOnDate != null)
     {
       totalValue += product.Price;
     }
@@ -151,8 +165,10 @@ void ListProducts()
   {
     Console.WriteLine($"{i + 1}. {products[i].Name}");
   }
+  Console.WriteLine($"Average Product Time in Stock: {AverageTimeStocked()} days");
+  Console.WriteLine($"Average Product Time on Shelf Before Sale: {AverageTimeBeforeSold()} days");
+  BusiestSaleHours();
 }
-
 
 void ViewLatestProducts()
 {
@@ -160,7 +176,7 @@ void ViewLatestProducts()
   DateTime threeMonthsAgo = DateTime.Now - TimeSpan.FromDays(90);
   foreach (Product product in products)
   {
-    if (product.StockDate > threeMonthsAgo && !product.Sold)
+    if (product.StockDate > threeMonthsAgo && product.SoldOnDate != null)
     {
       latestProducts.Add(product);
     }
@@ -171,6 +187,96 @@ void ViewLatestProducts()
   }
 }
 
+void MonthlySalesReport()
+{
+  Console.WriteLine("Input the year for the report you want as YYYY");
+  int userInputYear = int.Parse(Console.ReadLine());
+  Console.WriteLine("Input the month for the report you want as MM");
+  int userInputMonth = int.Parse(Console.ReadLine());
+
+  IEnumerable<Product> foundProducts = products.Where(product => 
+  {
+    if (product.SoldOnDate != null)
+    {
+      return  product.SoldOnDate.Value.Year == userInputYear &&
+              product.SoldOnDate.Value.Month == userInputMonth;
+    }
+    return false;
+  });
+
+  decimal totalPrice = foundProducts.Sum(product => product.Price);
+  Console.WriteLine($"Total sales for products sold in {userInputMonth}/{userInputYear}: ${totalPrice}");
+}
+
+void AddProduct()
+{
+  Console.WriteLine("Please enter the details of the product to add:");
+  Console.WriteLine("What is the name of the product?");
+  string productNameToAdd = Console.ReadLine();
+  Console.WriteLine("What is the price of the product?");
+  decimal productPriceToAdd = decimal.Parse(Console.ReadLine());
+  Console.WriteLine("What is the manufacture year of the product?");
+  int productManufactureYearToAdd = int.Parse(Console.ReadLine());
+  Console.WriteLine("What is the condition of the product on a scale of 0.0 - 5.0?");
+  double productConditionToAdd = double.Parse(Console.ReadLine());
+
+  DateTime now = new DateTime();
+  Product productToAdd = new Product()
+  {
+    Name = productNameToAdd,
+    Price = productPriceToAdd,
+    ManufactureYear = productManufactureYearToAdd,
+    Condition = productConditionToAdd,
+    StockDate = now,
+    SoldOnDate = null
+  };
+
+  products.Add(productToAdd);
+  Console.WriteLine($"Your {productNameToAdd} was successfully added.");
+
+  //TODO test add product
+}
+
+void MarkAsSold(int userInput)
+{
+  products[userInput - 1].SoldOnDate = new DateTime();
+}
+
+TimeSpan AverageTimeStocked()
+{
+  // Allow the user to see the average time that currently stocked products have been on the shelf (in days).
+  TimeSpan totalTime = new TimeSpan(products.Sum(p => p.TimeInStock.Ticks));
+  return totalTime / (double)products.Count;
+}
+
+TimeSpan AverageTimeBeforeSold()
+{
+  // Allow the user to see the average amount of time the sold products were on the shelf before sale.
+  var soldProducts = products.Where(p => p.SoldOnDate != null);
+  TimeSpan totalTime = new TimeSpan(soldProducts.Sum(p => p.TimeInStock.Ticks));
+  int soldProductsCount = soldProducts.Count();
+  return totalTime / (double)soldProductsCount;
+}
+
+void BusiestSaleHours()
+{
+  List<Product> soldProducts = products.Where(p => p.SoldOnDate != null).ToList();
+  int[] storeHours = { 9, 10, 11, 12, 1, 2, 3, 4 };
+  // iterate through store hours 9-4
+  foreach (int h in storeHours)
+  {
+    int numberSold = 0;
+    foreach (Product p in soldProducts)
+    {
+      if (h == p.SoldOnDate.Value.Hour)
+      {
+        numberSold++;
+      }
+    }
+    Console.WriteLine($"{h}:00 -- {numberSold} items sold");
+  }
+  // if product was sold in that hours, console it
+}
 
 // Console.WriteLine("Please enter a product number: ");
 // int response = int.Parse(Console.ReadLine().Trim());
